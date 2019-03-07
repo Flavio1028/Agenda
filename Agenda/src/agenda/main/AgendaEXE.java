ae16cd3bc6a8b5562c06d1f22eaeb0fc6dd15a42
flavio.rocha <Flvio Rocha>
2019-03-07 14:39:32 -0300

    Ajuste.

--- Agenda/src/agenda/main/AgendaEXE.java
+++ Agenda/src/agenda/main/AgendaEXE.java
@@ -1,13 +1,19 @@
 package agenda.main;
+
 import javax.swing.*;
 
 import agenda.view.InterfaceGUI;
 
-public class AgendaEXE
-{
-   public static void main(String[]args)
-   {
-      InterfaceGUI g = new InterfaceGUI();
-      g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   }
+/**
+ * @author flavio.rocha
+ */
+public class AgendaEXE {
+
+	public static void main(String[] args) {
+
+		InterfaceGUI g = new InterfaceGUI();
+		g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
 }
